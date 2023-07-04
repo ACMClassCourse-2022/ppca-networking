eff841e713e0f822b4f4a62c58d15af03dba7731
Alan Liang <symb@olic.link>
2023-07-04 16:33:50 +0800

    tests(base): testing for concurrency

--- nsutils/lib/testdrv.c
+++ nsutils/lib/testdrv.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,26 +8,49 @@
 void check (int res, const char *msg) {
   if (res) {
     perror(msg);
-    exit(1);
+    exit(2);
+  }
+}
+
+void swap (int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+void shuffle (int *array, int length) {
+  for (int i = 0; i < length; ++i) {
+    swap(&array[i], &array[i + (rand() % (length - i))]);
   }
 }
 
 int main (int argc, char **argv) {
-  int pipefd[2][2];
-  check(pipe(pipefd[0]), "pipe");
-  check(pipe(pipefd[1]), "pipe");
-  pid_t pid = fork();
-  check(pid < 0, "fork");
-  if (!pid) {
-    check(dup2(pipefd[0][0], 0) < 0, "dup2");
-    check(dup2(pipefd[1][1], 1) < 0, "dup2");
-    argv[0] = "nc";
-    check(execvp("nc", argv), "execvp");
+  int parallel;
+  scanf("%d\n",&parallel);
+  int pipefd[parallel][2][2];
+  int perm[parallel];
+  pid_t pid[parallel];
+  for (int i = 0; i < parallel; ++i) {
+    perm[i] = i;
+    check(pipe(pipefd[i][0]), "pipe");
+    check(pipe(pipefd[i][1]), "pipe");
+    pid[i] = fork();
+    check(pid[i] < 0, "fork");
+    if (!pid[i]) {
+      check(dup2(pipefd[i][0][0], 0) < 0, "dup2");
+      check(dup2(pipefd[i][1][1], 1) < 0, "dup2");
+      argv[0] = "nc";
+      check(execvp("nc", argv), "execvp");
+    }
   }
   while (1) {
+    shuffle(perm, parallel);
     char cmd[10];
     int res = scanf("%8s", cmd);
     if (!res || res == EOF) {
+      for (int i = 0; i < parallel; ++i) {
+        check(kill(pid[i], SIGTERM), "kill");
+      }
       return 0;
     } else if (!strcmp(cmd, "send")) {
       int len;
@@ -38,25 +62,31 @@ int main (int argc, char **argv) {
         scanf("%d", &v);
         buf[i] = v;
       }
-      write(pipefd[0][1], buf, len);
+      for (int i = 0; i < parallel; ++i) {
+        write(pipefd[perm[i]][0][1], buf, len);
+      }
     } else if (!strcmp(cmd, "recv")) {
       int len;
       scanf("%d", &len);
       uint8_t *buf = malloc(len);
       check(!buf, "malloc");
       for (int i = 0; i < len; ++i) {
-        int len = read(pipefd[1][0], &buf[i], 1);
-        if (!len) {
-          printf("read ended prematurely\n");
-          return 1;
-        }
-      }
-      for (int i = 0; i < len; ++i) {
         int c;
         scanf("%d", &c);
-        if (c != buf[i]) {
-          printf("wrong value received at %d; expected %d, got %d\n", i, c, buf[i]);
-          return 1;
+        buf[i] = c;
+      }
+      for (int i = 0; i < parallel; ++i) {
+        for (int j = 0; j < len; ++j) {
+          uint8_t c;
+          int len = read(pipefd[perm[i]][1][0], &c, 1);
+          if (!len) {
+            printf("read ended prematurely\n");
+            return 1;
+          }
+          if (c != buf[j]) {
+            printf("wrong value received at %d; expected %d, got %d\n", j, buf[j], c);
+            return 1;
+          }
         }
       }
     } else if (!strcmp(cmd, "sleep")) {
